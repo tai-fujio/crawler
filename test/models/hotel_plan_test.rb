dc4889ecc41ece3880d0dcd5198f2578517aa50a
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-07-08 19:36:52 +0900

    gem vcr,webmock

--- test/models/hotel_plan_test.rb
+++ test/models/hotel_plan_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
+require 'vcr'
 
 class HotelPlanTest < ActiveSupport::TestCase
   test ".fetch_hotel_plan" do
-    HotelPlan.fetch_hotel_plan
+    VCR.use_cassette('fetch_hotel') do
+      HotelPlan.fetch_hotel_plan
+    end
   end
 end
